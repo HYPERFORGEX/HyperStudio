1a869c466801ead00732e3b3a3004789beaac8ad
HYPERFORGEX <hyperforgex@gmail.com>
2026-06-18 22:53:53 +0530

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,1 +1,17 @@
-// TODO SQLite user model
+const db = require("../db/database");
+const bcrypt = require("bcryptjs");
+
+const User = {
+    createUser: (username, password) => {
+        const hash = bcrypt.hashSync(password, 10);
+        const stmt = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+        return stmt.run(username, hash);
+    },
+
+    findByUsername: (username) => {
+        const stmt = db.prepare("SELECT * FROM users WHERE username = ?");
+        return stmt.get(username);
+    }
+};
+
+module.exports = User;
